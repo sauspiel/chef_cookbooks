dff4b301590b2e48f65dee948f478035fa86d2ce
Holger Amann <holger@sauspiel.de>
2012-04-04 21:57:43 +0200

    apt: removing ubuntu specific stuff

--- apt/recipes/default.rb
+++ apt/recipes/default.rb
@@ -2,10 +2,3 @@ execute "apt-get-update" do
   command "apt-get update"
   action :nothing
 end
-
-# run this to grab GPG keys for enabled ppas
-
-cookbook_file "/usr/local/bin/launchpad-update" do
-  source "launchpad-update.sh"
-  mode 0755
-end
