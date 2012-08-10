0cc0e709ff0aac3fca193e83b759612671494861
Joshua Sierles <joshua@diluvia.net>
2012-08-10 13:23:39 +0200

    remove cruft from memcached recipe

--- memcached/attributes/memcached.rb
+++ memcached/attributes/memcached.rb
@@ -1,6 +1,6 @@
 default.memcached[:conf_path] = "/etc/memcached.conf"
-default.memcached[:max_memory] = 256 unless memcached.has_key?(:max_memory)
-default.memcached[:max_connections] = 1024 unless memcached.has_key?(:max_connections)
-default.memcached[:port] = 11211 unless memcached.has_key?(:port)
-default.memcached[:user] = "nobody" unless memcached.has_key?(:user)
+default.memcached[:max_memory] = 256
+default.memcached[:max_connections] = 1024
+default.memcached[:port] = 11211
+default.memcached[:user] = "nobody"
 default.memcached[:log_path] = "/var/log/memcached.log"
