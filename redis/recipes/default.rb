e61d6c11000a32f05387b529d7649fdd6b8a89c1
Holger Amann <holger@sauspiel.de>
2012-09-04 13:59:08 +0200

    redis: disabling redis service

--- redis/recipes/default.rb
+++ redis/recipes/default.rb
@@ -2,3 +2,16 @@ package 'redis-server' do
   version node[:redis][:version]
   action :install
 end
+
+service "redis-server" do
+  action :stop
+end
+
+execute "disabling redis-server service" do
+  command "/usr/sbin/update-rc.d -f redis-server remove >/dev/null 2>&1"
+  action :run
+  ignore_failure true
+end
+
+
+
