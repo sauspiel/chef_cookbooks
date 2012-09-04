879dc89544058ee45c1111e3edc030c2e079bb89
Holger Amann <holger@sauspiel.de>
2012-09-04 17:47:14 +0200

    redis: using insserv provider for removing service

--- redis/recipes/default.rb
+++ redis/recipes/default.rb
@@ -4,14 +4,6 @@ package 'redis-server' do
 end
 
 service "redis-server" do
-  action :stop
+  provider Chef::Provider::Service::Insserv
+  action [:disable, :stop]
 end
-
-execute "disabling redis-server service" do
-  command "/usr/sbin/update-rc.d -f redis-server remove >/dev/null 2>&1"
-  action :run
-  ignore_failure true
-end
-
-
-
