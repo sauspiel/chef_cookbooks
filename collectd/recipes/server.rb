941ebb56beae04f12d1f6305970c788c49175a23
Holger Amann <holger@sauspiel.de>
2012-09-21 12:30:15 +0200

    collectd: removing network.conf in collectd-server recipe

--- collectd/recipes/server.rb
+++ collectd/recipes/server.rb
@@ -1,5 +1,9 @@
 include_recipe "collectd::default"
 
+file "#{node[:collectd][:conf_dir]}/conf.d/network.conf" do
+  action :delete
+end
+
 collectd_plugin "network" do
   config :listen=>'0.0.0.0'
   name "network_server"
