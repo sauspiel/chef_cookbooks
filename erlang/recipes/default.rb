7ad757c6251ec709960897c3bd37bb578d91b141
Holger Amann <holger@sauspiel.de>
2012-10-05 13:04:54 +0200

    erlang: installation fix

--- erlang/recipes/default.rb
+++ erlang/recipes/default.rb
@@ -1,5 +1,5 @@
-%w(erlang-base-hipe erlang-nox) do |pkg|
-  apt_package pkg do
+%w(erlang-base-hipe erlang-nox).each do |pkg|
+  apt_package "#{pkg}" do
     default_release node[:erlang][:debian_release]
     version node[:erlang][:version]
   end
