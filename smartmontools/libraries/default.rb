ace2dc8ef2a11d3a4dcba0a90932267e79d78926
Holger Amann <holger@sauspiel.de>
2012-11-22 16:29:05 +0100

    smartmontools: using sat+megaraid,#deviceid

--- smartmontools/libraries/default.rb
+++ smartmontools/libraries/default.rb
@@ -4,7 +4,7 @@ def smartmontools_megaraid_devices
   devices = Array.new
   ctrl = node[:raid_controllers].values.select { |ctrl| ctrl['type'] =~ /MegaRAID/ }.first
   ctrl['physical_disks'].values[0].each do |k,device|
-    devices << "sda -d megaraid,#{device['Device Id']}"
+    devices << "sda -d sat+megaraid,#{device['Device Id']}"
   end
   return devices
 end
