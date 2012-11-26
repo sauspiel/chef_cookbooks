a429a99cb78438de6014bff5de3552a9e13760b6
Joshua Sierles <joshua@diluvia.net>
2012-11-26 20:08:30 +0100

    make bluepill version a default attribute

--- bluepill/attributes/bluepill.rb
+++ bluepill/attributes/bluepill.rb
@@ -5,6 +5,6 @@ default.bluepill[:init_dir] = "/etc/init.d"
 default.bluepill[:conf_dir] = "/etc/bluepill"
 default.bluepill[:log_dir] = "/var/log/bluepill"
 default.bluepill[:pid_dir] = "/var/run/bluepill"
-default[:bluepill][:state_dir] = "/var/lib/bluepill"
+default.bluepill[:state_dir] = "/var/lib/bluepill"
 
-bluepill[:version] = "0.0.60"
+default.bluepill[:version] = "0.0.60"
