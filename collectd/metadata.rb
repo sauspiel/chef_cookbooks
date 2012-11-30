b82efe33bae78216685fea7eff6561606fb992d5
Holger Amann <holger@sauspiel.de>
2012-11-30 09:47:38 +0100

    collectd: using "##" as delimiter

--- collectd/metadata.rb
+++ collectd/metadata.rb
@@ -4,5 +4,5 @@ maintainer_email "nkantrowitz@crypticstudios.com"
 description      "Install and configure the collectd monitoring daemon and plugins"
 license          "Apache 2.0"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.2.4"
+version          "1.2.5"
 supports         "debian"
