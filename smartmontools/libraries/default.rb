35feb42195545e3fc520d36d3150a98ce8dec542
Holger Amann <holger@sauspiel.de>
2012-12-13 17:58:53 +0100

    smartmontools: adding auto detecting of devices for megaraid controllers which identify as MegaSAS

--- smartmontools/libraries/default.rb
+++ smartmontools/libraries/default.rb
@@ -2,7 +2,7 @@ def smartmontools_megaraid_devices
   # ohai plugin for megaraid is required!
   # => https://github.com/gpapilion/raid_controllers_ohai
   devices = Array.new
-  ctrl = node[:raid_controllers].values.select { |ctrl| ctrl['type'] =~ /MegaRAID/ }.first
+  ctrl = node[:raid_controllers].values.select { |ctrl| ctrl['type'] =~ /(MegaRAID|MegaSAS)/ }.first
   ctrl['physical_disks'].values[0].each do |k,device|
     devices << "sda -d sat+megaraid,#{device['Device Id']}"
   end
