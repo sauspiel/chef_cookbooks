15dd4ec8efc6d26b060029f767b85f4d1cd74a5b
Holger Amann <holger@sauspiel.de>
2013-01-09 11:06:56 +0100

    ssh: minor fix

--- ssh/recipes/server.rb
+++ ssh/recipes/server.rb
@@ -19,6 +19,7 @@ else
     addresses = node[:ssh][:addresses]
   else
     node[:network][:interfaces].each do |iface, addrs|
+      next if addrs[:addresses].nil?
       addrs[:addresses].each do |ip, params|
         addresses << ip if params[:family].eql?('inet')
       end
