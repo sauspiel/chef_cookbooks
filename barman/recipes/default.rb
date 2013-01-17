7321037a6043eff0520b2f5b5595106908f1bea0
Holger Amann <holger@sauspiel.de>
2013-01-17 14:05:29 +0100

    barman: updating repository url

--- barman/recipes/default.rb
+++ barman/recipes/default.rb
@@ -1,6 +1,6 @@
 apt_repository "pgdg" do
-  uri "http://pgapt.debian.net" 
-  distribution "squeeze-pgdg"
+  uri "http://apt.postgresql.org/pub/repos/apt" 
+  distribution "#{node[:lsb][:codename]}-pgdg"
   components ["main"]
   keyserver "keys.gnupg.net"
   key "ACCC4CF8"
