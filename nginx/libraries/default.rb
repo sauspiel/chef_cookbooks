2b58114062e78ab4d4af5f955f9c177c3e29d70d
Holger Amann <holger@sauspiel.de>
2013-01-22 15:17:29 +0100

    nginx: adding method to check if nginx is build with spdy

--- nginx/libraries/default.rb
+++ nginx/libraries/default.rb
@@ -5,3 +5,7 @@ def bind_address_to_s(port)
     return "#{node[:network][:interfaces][node[:nginx][:bind_interface]][:addresses].select { |address, data| data["family"] == "inet" }[0][0]}:#{port}"
   end
 end
+
+def nginx_spdy_support?
+  return `nginx -V 2>&1`.include?("--with-http_spdy_module")
+end
