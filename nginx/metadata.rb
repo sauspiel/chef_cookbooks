980e32522b5c9b7b1a44791b9dbe7fc285fa4907
Holger Amann <holger@sauspiel.de>
2013-02-06 15:48:18 +0100

    nginx: creating sites-include dir

--- nginx/metadata.rb
+++ nginx/metadata.rb
@@ -2,6 +2,6 @@ maintainer        "Joshua Sierles"
 maintainer_email  "joshua@diluvia.net"
 license           "Apache 2.0"
 description       "Installs and configures nginx"
-version           "1.0.12"
+version           "1.0.13"
 
 depends "apt"
--- nginx/recipes/default.rb
+++ nginx/recipes/default.rb
@@ -48,7 +48,7 @@ directory node[:nginx][:log_dir] do
   action :create
 end
 
-%w(sites-available sites-enabled helpers site-include common htpasswd.d).each do |dir|
+%w(sites-available sites-enabled helpers sites-include common htpasswd.d).each do |dir|
   directory "#{node[:nginx][:dir]}/#{dir}" do
     owner "root"
     group "root"
