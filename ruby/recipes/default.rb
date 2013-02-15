cc7790a0c671be8fe8bfc59c0d6f0cd9c846c211
Holger Amann <holger@sauspiel.de>
2013-02-15 14:48:53 +0100

    ruby: do not install ruby package everytime, only after download

--- ruby/recipes/default.rb
+++ ruby/recipes/default.rb
@@ -5,15 +5,17 @@ tmp = node[:tmp] ? node[:tmp] : "/tmp"
 rubypkg = "ruby-#{node.ruby.version}.deb"
 debpath = "#{tmp}/#{rubypkg}"
 
+dpkg_package "ruby" do
+  version node.ruby.version.gsub("_amd64","")
+  source "#{debpath}"
+  action :nothing
+end
+
 remote_file "#{debpath}" do
   source "#{node[:package_url]}/#{rubypkg}"
   not_if { File.exists?("#{debpath}") }
+  action :create
+  notifies :install, resource(:dpkg_package, "ruby"), :immediately
 end
 
-version = node.ruby.version.gsub("_amd64","")
-dpkg_package "ruby" do
-  version version
-  source "#{debpath}"
-  not_if { "dpkg-query -W ruby-#{version}" }
-  only_if { File.exists?("#{debpath}") }
-end
+
