c4479cb86b2a1f0c4ea94db29abdc8b7f61e63bc
Holger Amann <holger@sauspiel.de>
2013-02-18 20:38:49 +0100

    unicorn: extending unicorn_app to make it usable

--- unicorn/definitions/unicorn_app.rb
+++ unicorn/definitions/unicorn_app.rb
@@ -2,14 +2,43 @@ define :unicorn_app do
   include_recipe "unicorn"
   include_recipe "bluepill"
 
-  config_path = "#{node[:unicorn][:config_path]}/#{params[:name]}"
+  %w(cache pids unicorn).each do |dir|
+    directory "#{params[:app_root]}/tmp/#{dir}" do
+      owner params[:user]
+      group params[:user]
+      mode 0755
+      recursive true
+    end
+  end
+
+  config_path = params[:config_path] || "#{node[:unicorn][:config_path]}/#{params[:name]}.conf.rb"
 
   template config_path do
     cookbook 'unicorn'
-    source "unicorn.pill.erb"
+    source "unicorn.conf.erb"
+    owner params[:user]
+    group params[:group]
+    mode 0644
+    variables :app_name => params[:name], :app_root => params[:app_root], :worker_count => params[:worker_count], :preload => params[:preload]
   end
-  
+
+  template "#{node[:bluepill][:conf_dir]}/#{params[:name]}.pill" do
+    cookbook 'unicorn'
+    source "bluepill.conf.erb"
+    variables :app_name => params[:name], 
+      :app_root => params[:app_root], 
+      :environment_vars => params[:environment_vars] || Hash.new,
+      :memory_limit => params[:memory_limit] || 100, 
+      :user => params[:user] || "deploy", 
+      :group => params[:group] || "deploy", 
+      :cpu_limit => params[:cpu_limit] ||50,
+      :command => params[:command], 
+      :preload => params[:preload]
+  end
+
   bluepill_service params[:name] do
+    cookbook 'unicorn'
+    template 'bluepill.conf.erb'
     action [:enable, :load, :start]
   end
 end
