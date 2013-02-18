53d22ce8331f90c4784eaf00766da6e916dd0abd
Holger Amann <holger@sauspiel.de>
2013-02-18 20:44:05 +0100

    team_dashboard: creating configs for unicorn and bluepill

--- team_dashboard/attributes/default.rb
+++ team_dashboard/attributes/default.rb
@@ -1,3 +1,4 @@
 default.team_dashboard[:path] = '/var/www/team_dashboard'
 default.team_dashboard[:user] = 'deploy'
 default.team_dashboard[:group] = 'deploy'
+default.team_dashboard[:graphite_url] = "http://localhost/graphite"
--- team_dashboard/recipes/default.rb
+++ team_dashboard/recipes/default.rb
@@ -4,16 +4,6 @@ include_recipe "team_dashboard::database"
   package pkg
 end
 
-bash "modifying_gemfile" do
-  cwd node[:team_dashboard][:path]
-  user user
-  code <<-EOF
-  echo 'gem "unicorn", "~> 4.0.1"' >> Gemfile
-  EOF
-  not_if "grep -q unicorn Gemfile"
-  action :nothing
-end
-
 db_password = Chef::EncryptedDataBagItem.load('passwords', 'mysql')["dashboard_web"]
 app = search(:apps, "id:team_dashboard").first
 env = node.chef_environment
@@ -31,8 +21,19 @@ application "team_dashboard" do
   migrate true
   action :force_deploy
 
+  before_migrate do
+    bash "modifying_gemfile" do
+      cwd release_path
+      code <<-EOF
+      echo 'gem "unicorn", "~> 4.0.1"' >> Gemfile
+      EOF
+      not_if "grep -q unicorn Gemfile"
+    end
+  end
+
   rails do
     bundler true
+    bundler_deployment false
     database do
       database "team_dashboard_#{env}"
       username "dashboard_web"
@@ -40,5 +41,12 @@ application "team_dashboard" do
       adapter "mysql2"
       host "localhost"
     end
+  end
+
+  unicorn_bp do
+    worker_processes 2
+    preload_app true
+    worker_timeout 300
+    environment_vars :GRAPHITE_URL => node[:team_dashboard][:graphite_url]
   end
 end
