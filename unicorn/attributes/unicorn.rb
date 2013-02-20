7959217b1849ea7feebfb5ecd7b4edbf95260a04
Holger Amann <holger@sauspiel.de>
2013-02-20 08:48:02 +0100

    unicorn: creating run directory

--- unicorn/attributes/unicorn.rb
+++ unicorn/attributes/unicorn.rb
@@ -2,3 +2,6 @@ default.unicorn[:worker_count] = 6
 default.unicorn[:timeout] = 10
 default.unicorn[:version] = "4.0.1"
 default.unicorn[:config_path] = "/etc/unicorn"
+default.unicorn[:rundir] = "/var/run/unicorn"
+default.unicorn[:user] = 'deploy'
+default.unicorn[:group] = 'deploy'
--- unicorn/recipes/default.rb
+++ unicorn/recipes/default.rb
@@ -8,6 +8,12 @@ directory node[:unicorn][:config_path] do
   mode 0755
 end
 
+directory node[:unicorn][:rundir] do
+  mode 0755
+  owner node[:unicorn][:user]
+  group node[:unicorn][:user]
+end
+
 cookbook_file "/usr/local/bin/unicornctl" do
   mode 0755
 end
