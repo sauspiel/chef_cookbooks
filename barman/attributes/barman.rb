89c9c0726590e98356e9d4db4fa20c7a16f5fddb
Holger Amann <holger@sauspiel.de>
2013-02-27 00:03:33 +0100

    barman: adding rsync options patch for barman 1.2.0

--- barman/attributes/barman.rb
+++ barman/attributes/barman.rb
@@ -4,5 +4,5 @@ default.barman[:log_dir] = '/var/log/barman'
 default.barman[:databases] = []
 default.barman[:compression] = "None"
 default.barman[:manage_keys] = false
-default.barman[:version] = "1.1.2-1.sauspiel60+1"
+default.barman[:version] = "1.2.0-1.sauspiel60+1"
 
