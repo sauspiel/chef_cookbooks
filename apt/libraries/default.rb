d0d893ddba233dad88872bfe11206d0a1514e93f
Holger Amann <holger@sauspiel.de>
2013-02-27 18:00:11 +0100

    apt: using File class from stdlib

--- apt/libraries/default.rb
+++ apt/libraries/default.rb
@@ -7,7 +7,7 @@ end
 def apt_create_policy_rc_d_file
   file = "/usr/sbin/policy-rc.d"
   Chef::Log.info("Creating #{file}")
-  File.open(file, "w") do |f| 
+  ::File.open(file, "w") do |f| 
     f.puts "#!/bin/sh"
     f.puts "exit 101"
   end 
