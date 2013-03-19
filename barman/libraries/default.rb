9e63c438588b068f24c7410c60dcde0fa9262566
Holger Amann <holger@sauspiel.de>
2013-03-19 17:41:49 +0100

    barman: log if there is no master server for a configured database

--- barman/libraries/default.rb
+++ barman/libraries/default.rb
@@ -3,20 +3,24 @@ def barman_master_servers
   node[:barman][:databases].each do |name, conf|
 
     master = search(:node, "postgresql_role:master AND chef_environment:#{node.chef_environment} AND postgres_databases:#{name}").first
-    eth = conf[:ssh_master_eth] || master[:postgresql][:interfaces].reject { |i| i == "lo" }.first
-    x = Hash.new
-    x[:id] = name
-    x[:ip] = master[:network][:interfaces][eth][:addresses].select { |address, data| data[:family] == "inet"}[0][0]
+    if master.nil?
+      Chef::Log.warn("Could not find any master servers for database #{name} in #{node.chef_environment} environment")
+    else
+      eth = conf[:ssh_master_eth] || master[:postgresql][:interfaces].reject { |i| i == "lo" }.first
+      x = Hash.new
+      x[:id] = name
+      x[:ip] = master[:network][:interfaces][eth][:addresses].select { |address, data| data[:family] == "inet"}[0][0]
 
-    x[:ssh_master_address] = conf[:ssh_master_address] || x[:ip]
-    x[:pg_master_address] = conf[:pg_master_address] || x[:ip]
-    x[:pg_master_port] = conf[:pg_master_port] || 5432
+      x[:ssh_master_address] = conf[:ssh_master_address] || x[:ip]
+      x[:pg_master_address] = conf[:pg_master_address] || x[:ip]
+      x[:pg_master_port] = conf[:pg_master_port] || 5432
 
-    x[:name] = master[:fqdn]
+      x[:name] = master[:fqdn]
 
-    x[:rsync_options] = conf[:rsync_options]
+      x[:rsync_options] = conf[:rsync_options]
 
-    servers << x if servers.select { |f| f[:ip] == x[:ip] }.count == 0
+      servers << x if servers.select { |f| f[:ip] == x[:ip] }.count == 0
+    end
   end
   return servers
 end
