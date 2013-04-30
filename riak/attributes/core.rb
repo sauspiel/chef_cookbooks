924fa8d99fcfab7a9ffa494e7a1968a8ff4e8def
Holger Amann <holger@sauspiel.de>
2013-04-30 11:28:33 +0200

    riak: adding attribute for handoff_concurrency (default 2)

--- riak/attributes/core.rb
+++ riak/attributes/core.rb
@@ -41,3 +41,4 @@ default.riak.core.platform_etc_dir = "/etc/riak"
 default.riak.core.platform_data_dir = "/var/lib/riak"
 default.riak.core.ring_creation_size = 256
 default.riak.core.handoff_ip = node[:local_bind_address] || "0.0.0.0"
+default.riak.core.handoff_concurrency = 2
