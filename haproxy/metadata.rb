954a1d044f334b34d4668332469ee8c0cc34c46e
Holger Amann <holger@sauspiel.de>
2013-07-30 13:54:15 +0200

    haproxy: change ssl termination lwrp to support multiple frontends

--- haproxy/metadata.rb
+++ haproxy/metadata.rb
@@ -1,7 +1,7 @@
 maintainer        "Joshua Sierles"
 maintainer_email  "joshua@diluvia.net"
 description       "Configures haproxy"
-version           "0.4"
+version           "0.5"
 
 depends "apt"
 depends "logrotate"
