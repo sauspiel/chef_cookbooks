7f5294c42b907d3c9f46eb8584f581a842695bae
Holger Amann <holger@sauspiel.de>
2013-08-01 10:14:31 +0200

    cookbook for campy

---
+++ campy/attributes/default.rb
@@ -0,0 +1,2 @@
+default.campy[:version] = '1.0.0'
+default.campy[:bin] = "#{node['languages']['ruby']['bin_dir']}/campy"
---
+++ campy/metadata.rb
@@ -0,0 +1,5 @@
+name             'campy'
+maintainer       'Sauspiel GmbH'
+maintainer_email 'holger@sauspiel.de'
+description      'Installs/Configures campy'
+version          '0.0.1'
