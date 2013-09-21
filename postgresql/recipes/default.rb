cf90ccf4094358aefd14dd7b9b23f86ce5b2a7b1
Holger Amann <holger@sauspiel.de>
2013-09-21 10:53:05 +0200

    postgresql: add version as repository component if version is < 9.3
    this allows to install older postgresql versions

--- postgresql/recipes/default.rb
+++ postgresql/recipes/default.rb
@@ -1,7 +1,10 @@
+components = ["main"]
+components << node[:postgresql][:version] if node[:postgresql][:version].to_f < 9.3
+
 apt_repository "pgdg" do
   uri "http://apt.postgresql.org/pub/repos/apt"
   distribution "#{node[:lsb][:codename]}-pgdg"
-  components ["main"]
+  components components
   key "http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc"
   action :add
 end
