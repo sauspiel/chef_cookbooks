c30690ca68d170db0dd092687dd69432c13d4a47
Holger Amann <holger@sauspiel.de>
2013-10-04 09:59:22 +0200

    repmgr: bump to repmgr-1.2-2

--- repmgr/metadata.rb
+++ repmgr/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Holger Amann"
 maintainer_email "holger@sauspiel.de"
 description      "Installs/Configures repmgr"
-version          "0.0.5"
+version          "0.0.6"
 
 depends "postgresql"
 depends "bluepill"
