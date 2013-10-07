f65c73d95c61d2f9dd796075088964e7e7316cff
Holger Amann <holger@sauspiel.de>
2013-10-07 15:49:58 +0200

    haproxy: backend mode can be different to frontend mode

--- haproxy/metadata.rb
+++ haproxy/metadata.rb
@@ -1,7 +1,7 @@
 maintainer        "Joshua Sierles"
 maintainer_email  "joshua@diluvia.net"
 description       "Configures haproxy"
-version           "0.6"
+version           "0.7"
 
 depends "apt"
 depends "logrotate"
