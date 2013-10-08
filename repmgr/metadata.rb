9040f7fefd500dc6c0e5342baa0d60f9c33b8f01
Holger Amann <holger@sauspiel.de>
2013-10-08 13:57:20 +0200

    repmgr: change config hash to options hash due to changed collectd_plugin api

--- repmgr/metadata.rb
+++ repmgr/metadata.rb
@@ -1,7 +1,7 @@
 maintainer       "Holger Amann"
 maintainer_email "holger@sauspiel.de"
 description      "Installs/Configures repmgr"
-version          "0.0.6"
+version          "0.0.7"
 
 depends "postgresql"
 depends "bluepill"
--- repmgr/recipes/collectd.rb
+++ repmgr/recipes/collectd.rb
@@ -5,7 +5,7 @@ include_recipe "repmgr::default"
 collectd_plugin "postgres-slave" do
   template "collectd.erb"
   cookbook "repmgr"
-  config :user => node[:repmgr][:user], 
+  options :user => node[:repmgr][:user], 
           :cluster => node[:repmgr][:cluster], 
           :host => node[:repmgr][:host], 
           :dbname => node[:repmgr][:dbname],
