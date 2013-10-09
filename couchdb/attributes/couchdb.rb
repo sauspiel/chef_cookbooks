44287c4f21dc4e405fe1a3ed777d156a039f0788
Holger Amann <holger@sauspiel.de>
2013-10-09 11:48:54 +0200

    delete couchdb

--- couchdb/attributes/couchdb.rb
+++
@@ -1,3 +0,0 @@
-default.couchdb[:max_document_size] = "4294967296"
-default.couchdb[:port] = "5984"
-default.couchdb[:bind_address] = "127.0.0.1"
--- couchdb/metadata.rb
+++
@@ -1,4 +0,0 @@
-maintainer        "Joshua Sierles"
-maintainer_email  "joshua@diluvia.net"
-description       "Configures couchdb"
-version           "0.1"
