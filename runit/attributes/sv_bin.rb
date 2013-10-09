bc8d8c504c94dd66d00889cb7126a64dd9e7e6c4
Holger Amann <holger@sauspiel.de>
2013-10-09 11:40:28 +0200

    delete unused cookbooks ruby-shadow and runit

--- runit/attributes/sv_bin.rb
+++
@@ -1,4 +0,0 @@
-default.runit_sv_bin "/usr/bin/sv"
-
-default.runit_service_dir "/etc/service"
-default.runit_sv_dir "/etc/sv"
--- runit/metadata.rb
+++
@@ -1,4 +0,0 @@
-maintainer        "Joshua Sierles"
-maintainer_email  "joshua@diluvia.net"
-description       "Configures runit"
-version           "0.1"
