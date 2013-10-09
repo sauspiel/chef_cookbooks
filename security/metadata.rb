a66cd6234e58ea3c8d820cd885f20d7382a731ab
Holger Amann <holger@sauspiel.de>
2013-10-09 11:42:42 +0200

    add missing metadata.rb to sysctl and security

---
+++ security/metadata.rb
@@ -0,0 +1,5 @@
+maintainer "Sauspiel GmbH"
+maintainer_email "holger@sauspiel.de"
+description "Installs/Configures security"
+
+version "0.0.1"
---
+++ sysctl/metadata.rb
@@ -0,0 +1,4 @@
+maintainer "Sauspiel GmbH"
+maintainer_email "holger@sauspiel.de"
+description "Installs/Configures sysctl"
+version "0.0.1"
