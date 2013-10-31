5a26a5a17155f2b6c1dc40a4435c717db798b534
Holger Amann <holger@sauspiel.de>
2013-10-31 15:54:54 +0100

    smartmontools: it shouldn't fail if physical disks couldn't be detected (maybe because of missing megacli command)

--- smartmontools/libraries/default.rb
+++ smartmontools/libraries/default.rb
@@ -3,8 +3,10 @@ def smartmontools_megaraid_devices
   # => https://github.com/gpapilion/raid_controllers_ohai
   devices = Array.new
   ctrl = node[:raid_controllers].values.select { |ctrl| ctrl['type'] =~ /(MegaRAID|MegaSAS)/ }.first
-  ctrl['physical_disks'].values[0].each do |k,device|
-    devices << "sda -d sat+megaraid,#{device['Device Id']}"
+  if ctrl['physical_disks']
+    ctrl['physical_disks'].values[0].each do |k,device|
+      devices << "sda -d sat+megaraid,#{device['Device Id']}"
+    end
   end
   return devices
 end
