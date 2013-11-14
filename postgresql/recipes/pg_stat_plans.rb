2b13b5b5bc9ca8af100e81fcf32178ed030dbdd6
Holger Amann <holger@sauspiel.de>
2013-11-14 14:41:43 +0100

    postgresql: install pgstatplans only if it's included in shared_preload_libraries

--- postgresql/recipes/pg_stat_plans.rb
+++ postgresql/recipes/pg_stat_plans.rb
@@ -1,1 +1,3 @@
-package "postgresql-#{node[:postgresql][:version]}-pgstatplans"
+if node[:postgresql][:shared_preload_libraries].include?("pg_stat_plans")
+  package "postgresql-#{node[:postgresql][:version]}-pgstatplans"
+end
