1451c106f64698c68dd14bce32c5889822a6ec3c
Holger Amann <holger@sauspiel.de>
2014-01-03 16:18:18 +0100

    nginx: improve supported ssl ciphers

--- nginx/metadata.rb
+++ nginx/metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Joshua Sierles"
 maintainer_email  "joshua@diluvia.net"
 license           "Apache 2.0"
 description       "Installs and configures nginx"
-version           "1.0.16"
+version           "1.0.17"
 
 depends "apt"
 depends "ssl_certificates"
