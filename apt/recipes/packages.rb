84ffe5501186da27634ffa6bbcf3e4e95afe1e96
Holger Amann <holger@sauspiel.de>
2014-02-11 12:41:51 +0100

    delete apt cookbook

--- apt/recipes/packages.rb
+++
@@ -1,9 +0,0 @@
-include_recipe 'apt'
-
-if node[:apt][:packages]
-  node[:apt][:packages].each do |group, packages|
-    packages.each do |name|
-      package name
-    end
-  end
-end
--- apt/resources/selections.rb
+++
@@ -1,8 +0,0 @@
-actions :allow_upgrade, :deny_upgrade
-
-def initialize(*args)
-  super
-  @action = :deny_upgrade
-end
-
-attribute :pkg, :kind_of => String, :name_attribute => true 
