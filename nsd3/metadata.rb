16dd04d076a0f018f1d4fbfb632b992b3b5d725b
Holger Amann <holger@sauspiel.de>
2014-02-12 12:37:55 +0100

    nds3: allow to add CNAME records without A records and multiple host entries
    now it is possible to add CNAME only records, e.g.
    {"host": [{"abc.github.io.": "", "alias": ["www"]}]}
    results in
    "www IN CNAME luschentreff.github.io."
    
    and we can define multiple host entries by appending "_", e.g.
    
    {"host": [{"@": "a.b.c"}, {"@_": "c.d.e"}]}
    results in
    "@ IN A a.b.c
     @ IN A c.d.e"

--- nsd3/metadata.rb
+++ nsd3/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Holger Amann"
 maintainer_email "holger@sauspiel.de"
 description      "Installs/Configures nsd3"
-version          "0.0.6"
+version          "0.0.7"
 
 depends "logrotate"
