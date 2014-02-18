42b98885c33f0d1e652f711791090d6e04e9a46b
Holger Amann <holger@sauspiel.de>
2014-02-18 14:26:36 +0100

    delete team_dashboard

--- team_dashboard/attributes/default.rb
+++
@@ -1,6 +0,0 @@
-default.team_dashboard[:path] = '/var/www/team_dashboard'
-default.team_dashboard[:user] = 'deploy'
-default.team_dashboard[:group] = 'deploy'
-default.team_dashboard[:graphite_url] = "http://localhost/graphite"
-default.team_dashboard[:repository] = "https://github.com/fdietz/team_dashboard.git"
-default.team_dashboard[:repository_revision] = "master"
--- team_dashboard/metadata.rb
+++
@@ -1,9 +0,0 @@
-name             'team_dashboard'
-maintainer       'Sauspiel GmbH'
-maintainer_email 'holger@sauspiel.de'
-description      'Installs/Configures team_dashboard'
-version          '0.0.1'
-
-depends "database"
-depends "percona"
-depends "application_ruby"
