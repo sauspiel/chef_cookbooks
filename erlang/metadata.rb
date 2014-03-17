1eb6ed8c76a49cff05de2927cadbc0c1066007f6
Holger Amann <holger@sauspiel.de>
2014-03-17 16:16:30 +0100

    increase sysctl and erlang cb versions to make riak cookbook happy

--- erlang/metadata.rb
+++ erlang/metadata.rb
@@ -1,6 +1,6 @@
 maintainer        "Joshua Sierles"
 maintainer_email  "joshua@diluvia.net"
 description       "Configures erlang"
-version           "0.7"
+version           "1.5.0"
 
 depends "apt"
--- sysctl/metadata.rb
+++ sysctl/metadata.rb
@@ -1,4 +1,4 @@
 maintainer "Sauspiel GmbH"
 maintainer_email "holger@sauspiel.de"
 description "Installs/Configures sysctl"
-version "0.0.1"
+version "0.3.5"
