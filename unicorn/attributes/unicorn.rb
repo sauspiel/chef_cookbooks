dafaea4f3a91e654cbb84b6d372307b99d5f4943
Holger Amann <holger@sauspiel.de>
2014-03-20 17:21:42 +0100

    unicorn: 4.8.2

--- unicorn/attributes/unicorn.rb
+++ unicorn/attributes/unicorn.rb
@@ -1,6 +1,6 @@
 default.unicorn[:worker_count] = 6
 default.unicorn[:timeout] = 10
-default.unicorn[:version] = "4.0.1"
+default.unicorn[:version] = "4.8.2"
 default.unicorn[:config_path] = "/etc/unicorn"
 default.unicorn[:rundir] = "/var/run/unicorn"
 default.unicorn[:user] = 'deploy'
--- unicorn/metadata.rb
+++ unicorn/metadata.rb
@@ -1,5 +1,5 @@
 maintainer        "Joshua Sierles"
 maintainer_email  "joshua@diuvia.net"
 description       "Configures unicorn"
-version           "0.4"
+version           "0.5"
 depends           "ruby"
