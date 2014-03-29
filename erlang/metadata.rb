fbe4511513d6169fef49108c9e09220c59ae6b95
Holger Amann <holger@sauspiel.de>
2014-03-29 13:56:40 +0100

    decrease erlang cb version to 1.5.0 to make riak cookbook happy

--- erlang/metadata.rb
+++ erlang/metadata.rb
@@ -1,6 +1,6 @@
 maintainer        "Joshua Sierles"
 maintainer_email  "joshua@diluvia.net"
 description       "Configures erlang"
-version           "1.6.0"
+version           "1.5.0"
 
 depends "apt"
