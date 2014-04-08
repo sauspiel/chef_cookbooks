bc3f05806b55e0d2a3a49091a2d2e88a418ff7b2
Holger Amann <holger@sauspiel.de>
2014-04-08 16:50:37 +0200

    nginx: bump version

--- nginx/metadata.rb
+++ nginx/metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Joshua Sierles"
 maintainer_email  "joshua@diluvia.net"
 license           "Apache 2.0"
 description       "Installs and configures nginx"
-version           "1.1.0"
+version           "1.2.0"
 
 depends "apt"
 depends "ssl_certificates"
