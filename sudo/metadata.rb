06026176c8303bacf98666299e36a883ba24cf88
Holger Amann <holger@sauspiel.de>
2014-04-24 10:55:00 +0200

    sudo: allow groups to use sudo without entering password

--- sudo/metadata.rb
+++ sudo/metadata.rb
@@ -1,5 +1,5 @@
 maintainer        "Joshua Sierles"
 maintainer_email  "joshua@diluvia.net"
 description       "Configures sudo"
-version           "0.4"
+version           "0.5"
 depends           "users"
