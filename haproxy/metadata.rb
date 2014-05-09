cd82d5d215d7c83e494ae24d0c708f5936f01d10
Joshua Sierles <joshua@diluvia.net>
2014-05-09 01:55:43 +0100

    run haproxy under runit instead of eye

--- haproxy/metadata.rb
+++ haproxy/metadata.rb
@@ -1,10 +1,11 @@
 maintainer        "Joshua Sierles"
 maintainer_email  "joshua@diluvia.net"
 description       "Configures haproxy"
-version           "1.0"
+version           "1.0.1"
 
 depends "apt"
 depends "logrotate"
 depends "rsyslog"
 depends "eye"
+depends 'runit'
 depends "ssl_certificates", ">= 0.4.0"
