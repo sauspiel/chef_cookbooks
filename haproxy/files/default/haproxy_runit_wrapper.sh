08b36bfa3dba20d357e20ae646793b6ed2dc8528
Joshua Sierles <joshua@diluvia.net>
2014-05-15 14:07:48 +0200

    better haproxy wrapper

--- haproxy/files/default/haproxy_runit_wrapper.sh
+++ haproxy/files/default/haproxy_runit_wrapper.sh
@@ -1,23 +1,35 @@
-#!/bin/sh
-set -e
+#!/bin/bash
 
-# start Unicorn in the background:
-COMMAND="/usr/sbin/haproxy -D -f /etc/haproxy/haproxy.cfg"
 
-`$COMMAND`
+function on_exit()
+{
+  echo "killing $HAPROXY_PID"
+  kill $HAPROXY_PID
+  echo "exiting"
+}
 
-# for sig in USR2 TERM
-# do
-#   trap $COMMAND' -sf $(cat /var/run/haproxy/haproxy.pid)' $sig
-# done
+function on_reload()
+{
+  echo "Reloading haproxy"
+  /usr/sbin/haproxy -D -f /etc/haproxy/haproxy.cfg -sf $HAPROXY_PID
+  sleep 1
+  NEW_HAPROXY_PID=`cat /var/run/haproxy/haproxy.pid`
+  echo "Old pid is: $HAPROXY_PID New pid is: $NEW_HAPROXY_PID"
+  HAPROXY_PID=$NEW_HAPROXY_PID
+}
 
-for sig in USR1 TTOU TTIN INT HUP QUIT PIPE
+export HAPROXY_SUPERVISOR_PID=$$
+
+trap on_exit EXIT
+trap on_reload USR2
+
+/usr/sbin/haproxy -D -f /etc/haproxy/haproxy.cfg
+
+HAPROXY_PID=`cat /var/run/haproxy/haproxy.pid`
+
+echo "supervisor pid: $HAPROXY_SUPERVISOR_PID haproxy pid: $HAPROXY_PID"
+
+while [ -e /proc/$HAPROXY_PID ]
 do
-  trap 'kill -'$sig' $(cat /var/run/haproxy/haproxy.pid)' $sig
-done
-
-# loop forever while haproxy is running
-while pkill -0 haproxy
-do
-   sleep 1
+  sleep 0.1
 done
