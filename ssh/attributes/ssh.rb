8cc164abde911b8417eef46f01adc5d9ecfbcea1
Holger Amann <holger@sauspiel.de>
2014-07-14 12:45:00 +0200

    ssh: attribute to disable sftp server

--- ssh/attributes/ssh.rb
+++ ssh/attributes/ssh.rb
@@ -1,3 +1,4 @@
 default.ssh[:passwordauthentication] = false
 default.ssh[:permitrootlogin] = false
 default.ssh[:useprivilegeseparation] = true
+default.ssh[:sftp_server_enabled] = true
--- ssh/metadata.rb
+++ ssh/metadata.rb
@@ -1,4 +1,4 @@
 maintainer        "Joshua Sierles"
 maintainer_email  "joshua@diluvia.net"
 description       "Configures ssh"
-version           "0.5.1"
+version           "0.5.2"
