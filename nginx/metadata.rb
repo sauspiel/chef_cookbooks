132d9d9ace7c85ad700a3c3f76cb19af649087e0
Holger Amann <holger@sauspiel.de>
2014-08-01 13:10:42 +0200

    nginx: include forwarded_client_ip.conf in proxy.conf

--- nginx/metadata.rb
+++ nginx/metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Joshua Sierles"
 maintainer_email  "joshua@diluvia.net"
 license           "Apache 2.0"
 description       "Installs and configures nginx"
-version           "1.3.0"
+version           "1.3.1"
 
 depends "apt"
 depends "ssl_certificates"
