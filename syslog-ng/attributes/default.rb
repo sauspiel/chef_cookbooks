71593efb37ab8c4632b553a560d64cfa1448e938
Holger Amann <holger@sauspiel.de>
2014-08-27 17:35:38 +0200

    delete cookbook syslog-ng

--- syslog-ng/attributes/default.rb
+++
@@ -1,3 +0,0 @@
-default.syslog_ng[:root] = "/u/logs"
-default.syslog_ng[:target_host] = '10.10.0.13'
-default.syslog_ng[:default_host] = '10.10.0.13'
--- syslog-ng/metadata.rb
+++
@@ -1,6 +0,0 @@
-name              'syslog-ng'
-maintainer        "Joshua Sierles"
-maintainer_email  "joshua@diluvia.net"
-description       "Configures syslog-ng"
-version           "0.2"
-depends           "logrotate"
