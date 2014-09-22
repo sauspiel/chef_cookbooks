d09e28af41e3eda4bc38f772ebe54be81f86c6aa
Holger Amann <holger@sauspiel.de>
2014-09-22 15:03:52 +0200

    ssh: disable ip reverse lookups

--- ssh/attributes/ssh.rb
+++ ssh/attributes/ssh.rb
@@ -2,3 +2,4 @@ default.ssh[:passwordauthentication] = false
 default.ssh[:permitrootlogin] = false
 default.ssh[:useprivilegeseparation] = true
 default.ssh[:sftp_server_enabled] = true
+default.ssh[:use_dns] = false
--- ssh/metadata.rb
+++ ssh/metadata.rb
@@ -2,4 +2,4 @@ name              'ssh'
 maintainer        "Joshua Sierles"
 maintainer_email  "joshua@diluvia.net"
 description       "Configures ssh"
-version           "0.5.2"
+version           "0.6.0"
