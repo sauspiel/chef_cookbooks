968265160ed61f9e5aa64527414ebfb3e2a8aac6
Holger Amann <holger@sauspiel.de>
2015-07-27 14:43:15 +0200

    remove week ssl ciphers

--- haproxy/attributes/haproxy.rb
+++ haproxy/attributes/haproxy.rb
@@ -6,4 +6,4 @@ default.haproxy[:server_timeout] = "300s"
 default.haproxy[:listen_port] = 80
 default.haproxy[:admin_port] = 81
 default.haproxy[:instances] = {}
-default.haproxy[:ssl_default_bind_ciphers] = '!ADH:HIGH:RC4+RSA:-SSLv2:-EXP:!RC4:!kEDH:!aNULL:!MD5:-LOW'
+default.haproxy[:ssl_default_bind_ciphers] = 'EECDH+AES128:EECDH+AES256:RSA+AES128:RSA+AES256:DES-CBC3-SHA'
