b593fd69e2fc94d18d2c7c7cf6d69859630db0e9
Holger Amann <holger@sauspiel.de>
2015-07-28 13:13:04 +0200

    nsd3: support for dkim

--- nsd3/metadata.rb
+++ nsd3/metadata.rb
@@ -2,6 +2,6 @@ name             "nsd3"
 maintainer       "Holger Amann"
 maintainer_email "holger@sauspiel.de"
 description      "Installs/Configures nsd3"
-version          "0.0.10"
+version          "0.0.11"
 
 depends "logrotate"
