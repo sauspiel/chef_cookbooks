9943a0dbfa77e137ed2d11daa45c515ffe8b6116
Holger Amann <holger@sauspiel.de>
2016-02-03 15:18:44 +0100

    postgresql: Add 'max_wal_size' attribute

--- postgresql/metadata.rb
+++ postgresql/metadata.rb
@@ -2,7 +2,7 @@ name 'postgresql'
 maintainer "Holger Amann"
 maintainer_email "holger@sauspiel.de"
 description "Configures postgresql"
-version "0.1.1"
+version "0.2.0"
 
 depends "apt"
 depends "dpkg_autostart"
