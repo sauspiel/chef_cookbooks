9e33859a14c8ccdf99669a0fda518b027be02ea5
Holger Amann <holger@sauspiel.de>
2016-02-29 08:02:25 +0100

    postgresql enable wal_log_hints

--- postgresql/metadata.rb
+++ postgresql/metadata.rb
@@ -2,7 +2,7 @@ name 'postgresql'
 maintainer "Holger Amann"
 maintainer_email "holger@sauspiel.de"
 description "Configures postgresql"
-version "0.2.0"
+version "0.2.1"
 
 depends "apt"
 depends "dpkg_autostart"
